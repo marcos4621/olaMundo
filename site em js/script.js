8d2461a5319558af6a3636b38900e6d8cf304246
Bruno Marcos de Oliveira <brunomarcos159951@gmail.com>
2021-03-21 10:16:41 -0300

    update commit

--- site em js/script.js
+++ site em js/script.js
@@ -2,3 +2,12 @@
 // console.log(idada)
 // let altura = 183;
 // console.log(altura);
+//camelcase
+//const ingressoAdulto = 28
+
+let nome = 'Bruno Marcos';
+let idade = '24';
+let altura = '183';
+console.log(nome);
+console.log(idade);
+console.log(altura);
